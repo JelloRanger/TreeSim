0bf7e67e46f13d5e1c5260b55c671738ec09cae1
Jacob Abramson <jacob@jabramson.com>
2015-07-06 20:33:46 -0400

    Cleaned up TreeSim's GUI code. Also optimized how images are loaded which seems to fix most performance problems.

--- src/TreeSim.java
+++ src/TreeSim.java
@@ -17,8 +17,10 @@ import javax.swing.JPanel;
  */
 public class TreeSim extends JPanel {
 	
-	static Board board;
-	static int size;
+	private static Board board;
+	private static int size;
+	private final int WIDTH = 16;
+	private final static int TICKS = 10;
 	
 	public static void main(String[] args) throws InterruptedException {
 		
@@ -36,13 +38,14 @@ public class TreeSim extends JPanel {
 		size = scan.nextInt();
 		scan.nextLine();
 		
+		// initialize GUI
 		JFrame window = new JFrame("TreeSim");
-		window.add(new TreeSim());
+		TreeSim treeSim = new TreeSim();
+		treeSim.setBackground(new Color(185, 122, 87));
+		window.add(treeSim);
 		window.pack();
 		window.setLocationRelativeTo(null); // center window on screen
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//Dimension d = new Dimension(10* (size+1), 10 * size);
-		//window.setPreferredSize(d);
 		window.setVisible(true);
 		
 
@@ -51,8 +54,10 @@ public class TreeSim extends JPanel {
 		board.populateTrees(0.5); // 50% of board will be filled with Trees
 		board.populateLumberjacks(0.1); // 10% of board will be filled with Lumberjacks
 		board.populateBears(0.02); // 2% of board will be filled with Bears
-		System.out.println(board.display());
-		window.repaint();
+		
+		//System.out.println(board.display());
+		
+		window.repaint(); // update GUI
 
 		scan.close();
 		
@@ -60,119 +65,97 @@ public class TreeSim extends JPanel {
 		while (board.getAge() < 4800) {
 			board.tick();
 			
-			Thread.sleep(25);
-			System.out.println(board.display());
+			Thread.sleep(TICKS);
+			//System.out.println(board.display());
 			
+			// update GUI
 			window.repaint();
 		}
 	
 		
 	}
 	
+	// set size of GUI window
 	public Dimension getPreferredSize() {
-		return new Dimension(16*(size)+10, 16*(size+1)+10);
+		return new Dimension(WIDTH*(size)+10, WIDTH*(size+1)+10);
 	}
 	
-	private void drawString(Graphics g, String text, int x, int y) {
-        g.setFont(new Font("Courier", Font.PLAIN, 12));
-		/*for (String line : text.split("\n"))
-            g.drawString(line, x, y += g.getFontMetrics().getHeight());*/
+	// draw the current iteration of the simulation
+	private void drawSim(Graphics g, String text, int x, int y) {
         
-        int l = g.getFontMetrics().getHeight();
+		g.setFont(new Font("Courier", Font.PLAIN, 12));
         
-        
-        String newString = "";
-        String letter = "";
+        String simulationText = "";
+       
         boolean firstTime= true;
         for (String line : text.split("\n")) {
         	
         	if (firstTime) {
+        		// display the current month
         		g.drawString(line,x,y);
         		firstTime = false;
         	}
         	else
-        		newString += line;
+        		// add the rest of the simulation text
+        		simulationText += line;
         }
         
+        // the current selected symbol for the entity
+        String symbol = "";
+        
+        // load the entity images
+        BufferedImage seedImg = null;
+    	BufferedImage treeImg = null;
+    	BufferedImage elderTreeImg = null;
+    	BufferedImage lumberjackImg = null;
+    	BufferedImage bearImg = null;
+        try {
+        	seedImg = ImageIO.read(new File("src/seed.png"));
+        	treeImg = ImageIO.read(new File("src/tree-75-16.gif"));
+        	elderTreeImg = ImageIO.read(new File("src/tree-49-16.gif"));
+        	lumberjackImg = ImageIO.read(new File("src/axe.gif"));
+        	bearImg = ImageIO.read(new File("src/bear-2-16.png"));
+        }
+        catch (IOException e) {
+        	e.printStackTrace();
+        }
+        
+        // iterate through all the entities and draw their respective icons
         for (int i= 0; i < size; i++) {
         	for (int j = 0; j < size; j++) {
-        		letter = Character.toString(newString.charAt(j+i*size));
         		
-        		BufferedImage image = null;
-        		
-        		switch (letter) {
+        		symbol = Character.toString(simulationText.charAt(j+i*size));
+        		switch (symbol) {
         		case ".":
-        			//g.setColor(Color.green);
-        			//g.drawString(letter, x + 7 * (j % size), y+l+ l * (i % (size)));
-        			try {
-						image = ImageIO.read(new File("src/seed.png"));
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-        			g.drawImage(image, x + 16 * (j % size), y+ 16 * (i % (size)), null);
+        			g.drawImage(seedImg, x + WIDTH * (j % size), y + WIDTH * (i % (size)), null);
         			break;
         		case "L":
-        			//g.setColor(Color.darkGray);
-        			//g.drawString(letter, x + 7 * (j % size), y+l+ l * (i % (size)));
-        			
-					try {
-						image = ImageIO.read(new File("src/axe.gif"));
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-        			g.drawImage(image, x + 16 * (j % size), y+ 16 * (i % (size)), null);
+        			g.drawImage(lumberjackImg, x + WIDTH * (j % size), y + WIDTH * (i % (size)), null);
         			break;
         		case "B":
-        			//g.setColor(Color.red);
-        			//g.drawString(letter, x + 7 * (j % size), y+l+ l * (i % (size)));
-					try {
-						image = ImageIO.read(new File("src/bear-2-16.png"));
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-        			g.drawImage(image, x + 16 * (j % size), y+ 16 * (i % (size)), null);
+        			g.drawImage(bearImg, x + WIDTH * (j % size), y + WIDTH * (i % (size)), null);
         			break;
         		case "^":
-        			//g.setColor(new Color(0, 219, 40));
-        			//g.fillRect(x + 7 * (j % size), y+ l * (i % (size)), 7, 17);
-        			try {
-						image = ImageIO.read(new File("src/tree-75-16.gif"));
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-        			g.drawImage(image, x + 16 * (j % size), y+ 16 * (i % (size)), null);
+        			g.drawImage(treeImg, x + WIDTH * (j % size), y + WIDTH * (i % (size)), null);
         			break;
         		case "$":
-        			//g.setColor(new Color(13, 119, 0));
-        			//g.fillRect(x + 7 * (j % size), y+ l * (i % (size)), 7, 17);
-        			try {
-						image = ImageIO.read(new File("src/tree-49-16.gif"));
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-        			g.drawImage(image, x + 16 * (j % size), y+ 16 * (i % (size)), null);
+        			g.drawImage(elderTreeImg, x + WIDTH * (j % size), y + WIDTH * (i % (size)), null);
         			break;
         		default:
-        			g.setColor(Color.black);
-        			g.drawString(letter, x + 16 * (j % size), y+ 16 * (i % (size)));
         			break;
         		}
         	}
         }
     }
 	
+	// return the simulation board
 	public static Board getBoard() {
 		return board;
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		drawString(g, getBoard().display(), 10, 10);
+		drawSim(g, getBoard().display(), 10, 10);
 	}
 	
 }
